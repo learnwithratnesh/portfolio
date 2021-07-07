9375c713f1d76bebc94442124c55c2447b0d927b
Ratnesh Tiwari <ratneshtanj26@gmail.com>
2021-07-07 08:33:12 +0530

    first commit

---
+++ build/precache-manifest.2cfbc41acf5b594a0d5bc7306cea666b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e8c814ba54a1992dc85",
+    "url": "/static/css/main.b5b5a0a6.chunk.css"
+  },
+  {
+    "revision": "7e8c814ba54a1992dc85",
+    "url": "/static/js/main.7e8c814b.chunk.js"
+  },
+  {
+    "revision": "455d3a4716702d5cd61c",
+    "url": "/static/js/1.455d3a47.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "803612c20185a9494d97ccab948de8d4",
+    "url": "/index.html"
+  }
+];
---
+++ src/resumeData.js
@@ -0,0 +1,107 @@
+let resumeData = {
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Ratnesh Tiwari",
+  role: "Frontend Developer",
+  linkedinId: "Your LinkedIn Id",
+  skypeid: "Your skypeid",
+  roleDescription:
+    "A technology-focused role that seeks to create digital software that entices the user into a seamless interaction between human and computer.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/ratnesh-tiwari-427ab57a/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/learnwithratnesh",
+      className: "fa fa-github",
+    },
+    {
+      name: "skype",
+      url: "https://github.com/learnwithratnesh",
+      className: "fa fa-twitter",
+    },
+  ],
+  aboutme:
+    "I am currently working as a Software Engineer at Mastercard. Previously, I worked as a Software Engineer at Zensar Technologies and I started my career with Tata Consultancy Services.I pursued my Bachelors Degree of Engineering with specialisation in Computer Science.",
+  address: "India",
+  website: "https://learnwithratnesh.github.io/aboutme/index.html",
+  education: [
+    {
+      UniversityName: "Rajiv Gandhi Proudyogiki Vishwavidyalaya",
+      specialization: "CSE",
+      MonthOfPassing: "Aug",
+      YearOfPassing: "2015",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Tata Consultancy Services",
+      specialization: "System Engineer",
+      MonthOfLeaving: "",
+      YearOfLeaving: "April 2016 to Oct 2018",
+      Achievements: "",
+    },
+    {
+      CompanyName: "Zensar",
+      specialization: "Software Development Engineer",
+      MonthOfLeaving: "",
+      YearOfLeaving: "October 2018 to Feb 2020",
+      Achievements: "",
+    },
+    {
+      CompanyName: "Mastercard",
+      specialization: "Senior Software Engineer",
+      MonthOfLeaving: "",
+      YearOfLeaving: "March 2020 to present",
+      Achievements: "",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
+
+export default resumeData;
